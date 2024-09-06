b32668f576c27208eafad792f46c389ea53477d6
lmangall <l.mangallon@gmail.com>
2024-09-06 17:26:17 +0200

    feat: button triggers setShowCard

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,21 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { CardComponent } from "@/components/ui/card";
 
 export default function Home() {
+  const [showCard, setShowCard] = useState(false);
+
+  const handleClick = () => {
+    setShowCard(!showCard);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <main className="flex flex-col items-center justify-center flex-grow">
-        <Button>Click me</Button>
-        <CardComponent />
+        <Button onClick={handleClick}>Click me</Button>
+        {showCard && <CardComponent />}
       </main>
       <footer className="flex items-center justify-center">Footer</footer>
     </div>
