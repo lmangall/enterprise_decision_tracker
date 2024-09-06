4bd9aa80eaf23d29686e699b5f52c9ae7abca6e4
lmangall <l.mangallon@gmail.com>
2024-09-07 01:10:34 +0200

    fix(NewGoal) new interface should match context

--- src/components/PostDecision.ts
+++ src/components/PostDecision.ts
@@ -2,20 +2,25 @@ import { useContext } from "react";
 import { DecisionContext } from "@/context/DecisionContext";
 
 interface NewGoal {
+  id: number;
+  golden_ticket: boolean;
   title: string;
   description: string;
-  measurableGoal: string;
+  measurable_goal: string;
   status: "Pending" | "Completed";
-  goalMet: boolean;
+  goal_met: boolean;
   comments?: string;
-  targetDate?: string;
-  goldenTicket: boolean;
+  goal_date?: string;
+  created_at: string;
+  updated_at: string;
 }
 
 // put on db
-export async function createGoal(newGoal: NewGoal) {
+export async function createGoal(newGoal: NewGoal): Promise<NewGoal> {
   try {
     console.log(newGoal);
+    // simulate returning the goal
+    return newGoal;
   } catch (error) {
     console.error(error);
     throw error;
