93d47cf9b08f0835d046a17081f7cabee0106184
lmangall <l.mangallon@gmail.com>
2024-09-06 17:20:57 +0200

    feat(card): render boilerplate card for future form

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,14 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
- 
+import { Button } from "@/components/ui/button";
+import { CardComponent } from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <main className="flex flex-col items-center justify-center flex-grow">
         <Button>Click me</Button>
+        <CardComponent />
       </main>
-      <footer className="flex items-center justify-center">
-        Footer        
-      </footer>
+      <footer className="flex items-center justify-center">Footer</footer>
     </div>
   );
 }
