f9150754c7277b685d6da4fe4b2eb88e7fbc4660
lmangall <l.mangallon@gmail.com>
2024-09-07 16:09:57 +0200

    fix: fix wrong apostrophe escaping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
                 <CardHeader>
                   <CardTitle>Decision Details</CardTitle>
                   <CardDescription>
-                    View the selected decision'&apos;s details
+                    View the selected decision&#39;s details{" "}
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
