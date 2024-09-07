787acc9a0191ab4b700c73275870a805657e3045
lmangall <l.mangallon@gmail.com>
2024-09-08 00:35:01 +0200

    wip(editDecision): add query

--- src/hooks/EditDecision.ts
+++ src/hooks/EditDecision.ts
@@ -2,12 +2,24 @@
 import { db } from "@vercel/postgres";
 import { Decision } from "@/types/decision";
 
-//TODO: validation
+//TODO: validation has to be client side (context), in a parent component
+// import { isDuplicateDecision } from "@/components/utils/validation";
+
 export async function editDecision(decision: Decision): Promise<boolean> {
   const client = await db.connect();
   try {
     const result = await client.sql`
-      DELETE FROM decisions
+      UPDATE decisions
+      SET
+        golden_ticket = ${decision.golden_ticket},
+        title = ${decision.title},
+        description = ${decision.description},
+        measurable_goal = ${decision.measurable_goal},
+        status = ${decision.status},
+        goal_met = ${decision.goal_met},
+        comments = ${decision.comments},
+        goal_date = ${decision.goal_date},
+        updated_at = NOW()
       WHERE id = ${decision.id}
       RETURNING id;
     `;
